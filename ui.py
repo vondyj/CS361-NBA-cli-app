29f73d1bb997be197d5d7e4ea9c9700296e3b516
Jillian Vondy <jmvondy@gmail.com>
2023-04-24 20:25:43 -0700

    Conference -> divisions functional.

--- ui.py
+++ ui.py
@@ -100,11 +100,49 @@ def conferences_divisions():
 
 
 def conferences_western():
-    print("placeholder")
+
+    print("\n")
+
+    # pass info to "conference division service" (what conference we would like to get the divisions of)
+    with open("conference-division-service.txt", "w") as conference_divisions_service_txt:
+        conference_divisions_service_txt.write("West")
+
+    # wait for standings service to get info and send it back
+    for i in tqdm(range(10), total=10, desc="Getting Western Conference Divisions"):
+        sleep(.2)
+
+    # read the data sent from conference division service
+    with open("conference-division-service.txt") as conference_divisions_service_txt:
+        data = conference_divisions_service_txt.read()
+
+    js_data = json.loads(data)
+
+    # display the standings table
+    headings = ["Division", "Teams"]
+    print(tabulate([(division) for division in js_data.items()], headers=headings, tablefmt="psql"))
 
 
 def conferences_eastern():
-    print("placeholder")
+
+    print("\n")
+
+    # pass info to "conference division service" (what conference we would like to get the divisions of)
+    with open("conference-division-service.txt", "w") as conference_divisions_service_txt:
+        conference_divisions_service_txt.write("East")
+
+    # wait for standings service to get info and send it back
+    for i in tqdm(range(10), total=10, desc="Getting Eastern Conference Divisions"):
+        sleep(.2)
+
+    # read the data sent from conference division service
+    with open("conference-division-service.txt") as conference_divisions_service_txt:
+        data = conference_divisions_service_txt.read()
+
+    js_data = json.loads(data)
+
+    # display the standings table
+    headings = ["Division", "Teams"]
+    print(tabulate([(division) for division in js_data.items()], headers=headings, tablefmt="psql"))
 
 
 def conference_standings():
@@ -131,36 +169,46 @@ def western_standings():
 
     print("\n")
 
+    # pass info to "standings service" (what conference we would like to get the standings for)
     with open("standings-service.txt", "w") as standings_service_txt:
         standings_service_txt.write("west")
 
+    # wait for standings service to get info and send it back
     for i in tqdm(range(10), total=10, desc="Getting Western Conference Standings"):
         sleep(.2)
 
+    # read the data sent from standings service
     with open("standings-service.txt") as standings_service_txt:
         data = standings_service_txt.read()
 
     js_data = json.loads(data)
 
-    print(tabulate([(k) for k in js_data.items()], tablefmt="psql"))
+    # display the standings table
+    headings = ["Position", "Team"]
+    print(tabulate([(position) for position in js_data.items()], headers=headings, tablefmt="psql"))
 
 
 def eastern_standings():
 
     print("\n")
 
+    # pass info to "standings service" (what conference we would like to get the standings for)
     with open("standings-service.txt", "w") as standings_service_txt:
         standings_service_txt.write("east")
 
+    # wait for standings service to get info and send it back
     for i in tqdm(range(10), total=10, desc="Getting Eastern Conference Standings"):
         sleep(.2)
 
+    # read the data sent from standings service
     with open("standings-service.txt") as standings_service_txt:
         data = standings_service_txt.read()
 
     js_data = json.loads(data)
 
-    print(tabulate([(k) for k in js_data.items()], tablefmt="psql"))
+    # display the standings table
+    headings = ["Position", "Team"]
+    print(tabulate([(position) for position in js_data.items()], headers=headings, tablefmt="psql"))
 
 
 def teams_menu():
